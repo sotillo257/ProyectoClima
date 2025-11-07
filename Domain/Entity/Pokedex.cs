92af3f1a50659fc32cb7bbd840c7580f0c9a6c97
fantasma <maeianapolly2007@gmail.com>
2025-11-07 12:47:14 -0400

    Prueba de commit

---
+++ Domain/Entity/Pokedex.cs
@@ -0,0 +1,23 @@
+﻿namespace Domain.Entity;
+
+public class Clima
+{
+    public string Pokemon { get; private set; }
+    public int Id { get; private set; }
+    
+
+    public Pokedex(string pokemon, int id)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(ciudad, nameof(pokemon));
+        
+        Pokemon = pokemon;
+        Id = id;
+    }
+
+    public static Pokedex Create(string pokemon, int id, string type)
+    {
+        return new (pokemon, id, type);
+    }
+    
+
+}
