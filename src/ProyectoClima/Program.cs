e4d3fa11f83c118d74cd9a20d7f8d2339fc426c5
JESUSSOTILLO\jesus <jesus_sotillo05@hotmail.es>
2025-11-16 12:42:18 +0100

    pr -ci

--- src/ProyectoClima/Program.cs
+++ src/ProyectoClima/Program.cs
@@ -47,11 +47,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseExceptionHandler();
 
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
